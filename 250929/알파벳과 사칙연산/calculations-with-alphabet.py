7fcce0905e2ff9cf1d460abaa5bafdb51cfcf34a
박동진 <82745129+minmunui@users.noreply.github.com>
2025-09-29 12:55:31 +0900

    Title: 알파벳과 사칙연산, Time: 82ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 250929/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 250929/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -1,28 +1,60 @@
+combs = []
+chars = ["a", "b", "c", "d", "e", "f"]
+opers = ["+", "-", "*"]
+
+def choose(cur, n, r):
+
+    if len(cur) >= r:
+        # print(f"cur : {cur}, r : {r}")
+        combs.append(cur)
+        return
+    
+    else:
+        for i in n:
+            cur = cur + [i]
+            choose(cur, n, r)
+            cur = cur[:-1]
+
+choose([], [1, 2, 3, 4], len(chars))
+
+# print(combs)
+
 expression = input()
 
-# Please write your code here.
-opers = ["+", "-", "*"]
-chars = ["a", "b", "c", "d", "e", "f"]
+def get_value(exp, values):
+    dict_value = {
+        "a" : values[0],
+        "b" : values[1],
+        "c" : values[2],
+        "d" : values[3],
+        "e" : values[4],
+        "f" : values[5],
+    }
+    last = None
+    result = 0
+    for i, char in enumerate(exp):
+        # print(f"char = {char}, result = {result}, ", end = "")
+        if i == 0:
+            result = dict_value[char]
+        elif char in chars:
+            if last == "+":
+                result += int(dict_value[char])
+            elif last == "-":
+                result -= int(dict_value[char])
+            elif last == "*":
+                result *= int(dict_value[char])
+        elif char in opers:
+            last = char
+        # print(f"after_result = {result}")
+    return result
 
-result = 0
-last = None
 
-for i, cur in enumerate(expression):
-    if i == 0:
-        result = 4
-    
-    elif cur in opers:
-        last = cur
+maxima = 0
+for comb in combs:
+    # print(f"comb : {comb}")
+    value = get_value(expression, comb)
+    # print(f"value : {value}")
+    if value > maxima:
+        maxima = value
 
-    elif cur in chars:
-        if last == "+":
-            result += 4
-        if last == "-":
-            result -= 1
-        if last == "*":
-            if result < 0:
-                result *= 1
-            else:
-                result *= 4
-
-print(result)
+print(maxima)
